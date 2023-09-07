5a5f78aa81b8ed72f39232ca9c22396c3ffd7808
Deborah <deborah.douillet@aum.bio>
2023-09-07 14:53:00 +0200

    routing angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,22 @@ import {RouterModule, Routes} from "@angular/router";
 import {AngularComponent} from "./components/angular/angular.component";
 import {GithubComponent} from "./components/github/github.component";
 import {AlgorithmsComponent} from "./components/algorithms/algorithms.component";
+import {
+  AngularIntroductionComponent
+} from "./components/angular/components/angular-introduction/angular-introduction.component";
+import {ComponentComponent} from "./components/angular/components/component/component.component";
 
 const routes: Routes = [
 
   // TODO : upgrade path architecture
 
   { path: '', component: AngularComponent},
-  { path: 'angular', component: AngularComponent},
+  { path: 'angular', component: AngularComponent,
+    children:[
+      {path: 'introduction', component: AngularIntroductionComponent },
+      {path: 'component', component: ComponentComponent, }
+    ]
+  },
   { path: 'github', component: GithubComponent},
   { path: 'algorithms', component: AlgorithmsComponent},
 ]

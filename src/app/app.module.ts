b6a72f7f0197fedf909d3a1980f6b53a080c5ff9
Deborah <deborahdev.io@gmail.com>
2025-04-21 10:52:19 +0200

    fix rename

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,15 @@
 import { NgModule, isDevMode } from '@angular/core';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { ReactiveFormsModule } from "@angular/forms";
+import {RouterModule} from "@angular/router";
+import {routes} from "@app/app.routes";
 
 @NgModule({
 
   imports: [
+    // routing
+    RouterModule.forRoot(routes, { useHash: true }),
+
     // pwa
     ServiceWorkerModule.register('ngsw-worker.js', {
       enabled: !isDevMode(),

7b7ca6c2fcfc71a4540fc6c189246d0d8d6463bf
Deborah <deborahdev.io@gmail.com>
2025-05-01 08:15:40 +0200

    routing tutorial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { BodyComponent } from './body/body.component';
-import {HeaderComponent} from "./shared/header/header.component";
+import { HeaderComponent } from "./shared/header/header.component";
 
 @Component({
   selector: 'app-root',

d870f312d1e3ecd82d6ae866861cc7e2ca3fa102
Deborah <deborahdev.io@gmail.com>
2025-05-14 07:48:19 +0200

    add part one software component

--- src/app/components/software-craft/software-craft.component.ts
+++ src/app/components/software-craft/software-craft.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import {RouterOutlet} from "@angular/router";
+import {PartOneComponent} from "@app/components/software-craft/part-one/part-one.component";
 
 @Component({
   selector: 'app-software-craft',
   standalone: true,
   imports: [
-    RouterOutlet
+    RouterOutlet,
+    PartOneComponent
   ],
   templateUrl: './software-craft.component.html',
   styleUrl: './software-craft.component.scss'

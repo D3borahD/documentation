3b169c19381bbdeb41d7135e6bffa12a29360a0a
Deborah <deborahdev.io@gmail.com>
2025-05-24 09:20:19 +0200

    update sidebar and architecture

---
+++ src/app/components/software-craft/bdd/bdd.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bdd',
+  standalone: true,
+  imports: [],
+  templateUrl: './bdd.component.html',
+})
+export class BddComponent {
+
+}

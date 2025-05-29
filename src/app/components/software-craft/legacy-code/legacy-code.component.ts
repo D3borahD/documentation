0e96b8327b8910931738a98f74af562a28e3d7ca
Deborah <deborahdev.io@gmail.com>
2025-05-29 09:14:23 +0200

    begin legacy code section

---
+++ src/app/components/software-craft/legacy-code/legacy-code.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-legacy-code',
+  standalone: true,
+  imports: [],
+  templateUrl: './legacy-code.component.html',
+})
+export class LegacyCodeComponent {
+
+}

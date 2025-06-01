f1fced3cf667f0d43a1910642eaae38ae94b6360
Deborah <deborahdev.io@gmail.com>
2025-06-01 09:03:30 +0200

    wip legacy code section

--- src/app/components/software-craft/legacy-code/legacy-code.component.ts
+++ src/app/components/software-craft/legacy-code/legacy-code.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import {CalloutSectionComponent} from "@app/shared/callout-section/callout-section.component";
+import {CodeSectionComponent} from "@app/shared/code-section/code-section.component";
 
 @Component({
   selector: 'app-legacy-code',
   standalone: true,
-  imports: [],
+  imports: [
+    CalloutSectionComponent,
+    CodeSectionComponent
+  ],
   templateUrl: './legacy-code.component.html',
   styleUrls: ['./legacy-code.component.scss']
 })

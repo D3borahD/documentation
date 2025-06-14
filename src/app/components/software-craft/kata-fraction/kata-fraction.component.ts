705ee0d6049e84ae83bfd0d3e1f5b2d187a617b4
Deborah <deborahdev.io@gmail.com>
2025-06-14 07:50:08 +0200

    beginning kata fraction session

---
+++ src/app/components/software-craft/kata-fraction/kata-fraction.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-kata-fraction',
+  standalone: true,
+  imports: [],
+  templateUrl: './kata-fraction.component.html',
+  styleUrl: './kata-fraction.component.scss'
+})
+export class KataFractionComponent {
+
+}

43efb41914a4dad0f127c313c22e62163a26ea19
Deborah <deborahdev.io@gmail.com>
2025-07-23 06:20:12 +0200

    add last modules

---
+++ src/app/components/software-craft/craft20/craft20.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-craft20',
+  standalone: true,
+  imports: [],
+  templateUrl: './craft20.component.html',
+  styleUrl: './craft20.component.scss'
+})
+export class Craft20Component {
+
+}
---
+++ src/app/components/software-craft/etat-esprit/etat-esprit.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-etat-esprit',
+  standalone: true,
+  imports: [],
+  templateUrl: './etat-esprit.component.html',
+  styleUrl: './etat-esprit.component.scss'
+})
+export class EtatEspritComponent {
+
+}
